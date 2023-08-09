c37ad0e86246e632c42175023b2248308539bfe5
Kirill Tila <gauter51@gmail.com>
2023-08-09 22:56:03 +0300

    fix migration

--- Sources/App/Migrations/CreateYandexFormMigration.swift
+++ Sources/App/Migrations/CreateYandexFormMigration.swift
@@ -48,35 +48,14 @@ struct CreateYandexFormMigration2: AsyncMigration {
 
     func prepare(on database: Database) async throws {
         try await database.schema(YandexForm.schema)
-            .id()
-            .field("created_at", .string)
-            .field("name", .string, .required)
-            .field("gender", .string, .required)
-            .field("age", .string, .required)
-            .field("timeZone", .string, .required)
-            .field("email", .string, .required)
-            .field("telegram", .string, .required)
-            .field("discord", .string, .required)
-            .field("timeWork", .string, .required)
-            .field("teamRole", .string, .required)
-            .field("experience", .string, .required)
-            .field("exampleWork", .string, .required)
-            .field("genres", .string, .required)
-            .field("about", .string, .required)
-            .field("from", .string, .required)
-            .field("ourWokrRead", .string, .required)
-            .field("reasonTo", .string, .required)
-            .field("reasonFrom", .string, .required)
-            .field("editorTest", .string)
-            .field("translatorEnTest", .string)
-            .field("translatorJpTest", .string)
-            .field("typesetterTest", .string)
-            .field("redrawTest", .string)
-            .create()
+            .field("created_at", .string, .sql(.default(Date.now.ISO8601Format())))
+            .update()
     }
 
     func revert(on database: Database) async throws {
-        try await database.schema(YandexForm.schema).delete()
+        try await database.schema(YandexForm.schema)
+            .deleteField("created_at")
+            .update()
     }
 
 }
